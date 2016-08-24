2ce1a5cb8f81e8e5d08d321fec8e89b8dd8cec17
Kate Wood <katewood611@gmail.com>
2016-08-24 12:12:33 -0700

    changes todos controller to use  syntax

--- js/todosController.js
+++ js/todosController.js
@@ -1,13 +1,12 @@
 angular.module('todosApp')
 	.controller('TodosController', TodosController)
 
-function TodosController($log) {
-	var self = this
-	self.add = addTodo
-	self.newTodo = {description: "", completed: false, image: ""}
-	self.leftToDo = leftToDo
+function TodosController($scope, $log) {
+	$scope.add = addTodo
+	$scope.newTodo = {description: "", completed: false, image: ""}
+	$scope.leftToDo = leftToDo
 
-	self.all = [
+	$scope.all = [
 		{description: "feed the cat", completed: true, image: "http://www.catster.com/wp-content/uploads/2015/06/cat-eats-steak.jpg"},
 		{description: "stake some vamps", completed: false, image: "http://vignette3.wikia.nocookie.net/buffy/images/2/2f/Buffy_staking_vampire_tabula_rasa.gif/revision/latest?cb=20140118003318"},
 		{description: "buy a new dress", completed: true, image: "http://all-womens-dresses.com/wp-content/uploads/2015/11/black-and-white-polka-dot-dress-1.jpg"},
@@ -15,16 +14,16 @@ function TodosController($log) {
 		{description: "be awesome", completed: true, image: "http://www.quotesvalley.com/images/23/when-im-sad-i-stop-being-sad-and-be-awesome-instead.jpg"}
 	]
 
-	$log.log(self.all)
+	$log.log($scope.all)
 
 	function addTodo() {
-		self.all.push(self.newTodo)
-		self.newTodo = {description: "", completed: false, image: ""}
+		$scope.all.push($scope.newTodo)
+		$scope.newTodo = {description: "", completed: false, image: ""}
 	}
 
 	function leftToDo() {
 		var count = 0
-		self.all.forEach(function(todo){
+		$scope.all.forEach(function(todo){
 			count += todo.completed ? 0 : 1
 		})
 		return count
